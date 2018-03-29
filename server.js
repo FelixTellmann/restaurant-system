7837b3faeececdcc3f64eac9c5d9a6ad48a67133
Felix Tellmann <felix@tellmann.co.za>
2018-03-29 12:33:41 +0200

    Initial Commit

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.use(cookieSession(cookieConfig));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(passport.initialize());
-
 app.use(passport.session());
 
 /*================ Passport User Identification ================*/
